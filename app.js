fcd73b3d33786109c152ba4dff0e8eedcb000f8e
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-12-11 06:07:18 -0800

    solut

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
 
-const port = process.env.port ||4000
+const port = process.env.port || 4000
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/users", require('./Router/UserRouter'))
 
-app.listen(port,()=> console.log(`Server Is Running ${port}`))
+app.listen(port, () => console.log(`Server Is Running ${port}`))
--- controllers/userContro.js
+++ controllers/userContro.js
@@ -1,14 +1,13 @@
 
 //==============================================================================Create  User=========================================>>
 const PostData = (req, res,) => {
-    res.json({
-        Message: "Create Users"
-    })
+    console.log(req.params.body)
+    res.json({ Message: "Create Users" })
 }
 //==============================================================================Updata User=========================================>>
 const UPdataData = (req, res,) => {
     res.json({ Message: `update user ${req.params.id}` })
-}
+} 
 //==============================================================================Delete User=========================================>>
 const DeleteData = (req, res,) => {
     res.json({ Message: `delete user ${req.params.id}` })
