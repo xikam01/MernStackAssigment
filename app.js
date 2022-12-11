dc3e0ed81c524727ec91b1de367f10b05d4aba2a
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-12-11 02:11:25 -0800

    Some solution

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const express = require('express')
+const express = require("express")
+const app = express();
 const dotenv = require('dotenv')
 dotenv.config();
-const port = process.env.port || 5000
-
-const app = express()
+const port = process.env.Port || 9999
 
 
 
 
 
-app.use('/api/assi', require('./Routes/UseRouter'))
 
 
 
 
 
-app.listen(port, () => console.log(`Server is Running on ${port}`))
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log('Server Is Running On ${Port}'));
